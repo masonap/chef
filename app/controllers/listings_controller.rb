248cb31750a92006c8da3d9fd8a026dfd06acaa6
masonap <masonap@gmail.com>
2014-09-12 10:49:27 -0400

    Added new listing glyph and partial

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -90,7 +90,7 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:name, :description, :price, :image)
+      params.require(:listing).permit(:name, :cuisine, :description, :price, :image)
     end
 
     def check_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109094129) do
+ActiveRecord::Schema.define(version: 20140911214738) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140109094129) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+    t.string   "cuisine"
   end
 
   create_table "orders", force: true do |t|
